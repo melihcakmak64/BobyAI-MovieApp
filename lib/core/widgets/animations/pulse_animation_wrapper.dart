d0fe95e280bfba86e5f4e958564eb799372970c1
Melih Çakmak <melihcakmak1755@gmail.com>
2025-12-31 12:15:59 +0300

    animation refactoring

--- lib/core/widgets/animations/pulse_animation_wrapper.dart
+++ lib/core/widgets/animations/pulse_animation_wrapper.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class PulseAnimation extends StatefulWidget {
   final Widget child;
   final bool isActive;
-  final double pulseScale; // Ne kadar genişlesin? (Örn: 1.08)
+  final double pulseScale;
   final Duration duration;
 
   const PulseAnimation({
@@ -20,31 +20,30 @@ class PulseAnimation extends StatefulWidget {
 
 class _PulseAnimationState extends State<PulseAnimation>
     with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _scaleX;
+  late final AnimationController _controller;
+  late final Animation<double> _scale;
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(vsync: this, duration: widget.duration);
 
-    _scaleX = Tween<double>(
+    _scale = Tween<double>(
       begin: 1.0,
       end: widget.pulseScale,
     ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
-
-    if (widget.isActive) {
-      _controller.repeat(reverse: true);
-    }
   }
 
   @override
-  void didUpdateWidget(PulseAnimation oldWidget) {
+  void didUpdateWidget(covariant PulseAnimation oldWidget) {
     super.didUpdateWidget(oldWidget);
-    if (widget.isActive && !oldWidget.isActive) {
-      _controller.repeat(reverse: true);
-    } else if (!widget.isActive && oldWidget.isActive) {
-      _controller.reverse(); // Durdururken başlangıç boyutuna döner
+
+    if (oldWidget.isActive != widget.isActive) {
+      if (widget.isActive) {
+        _controller.repeat(reverse: true);
+      } else {
+        _controller.animateTo(0.0, curve: Curves.easeOut);
+      }
     }
   }
 
@@ -57,16 +56,16 @@ class _PulseAnimationState extends State<PulseAnimation>
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-      animation: _scaleX,
-      builder: (context, child) {
-        return Transform(
+      animation: _scale,
+      child: widget.child,
+      builder: (_, child) {
+        return Transform.scale(
+          scaleX: _scale.value,
+          scaleY: 1.0,
           alignment: Alignment.center,
-          // Sadece X (genişlik) değişir, Y (yükseklik) 1.0'da sabit kalır
-          transform: Matrix4.diagonal3Values(_scaleX.value, 1.0, 1.0),
           child: child,
         );
       },
-      child: widget.child,
     );
   }
 }
